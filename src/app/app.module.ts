1ac0c0abd6d05cac0fb97a212862f28611b9f9a8
CIST2932Spring2019 <william.ith13@gmail.com>
2019-02-12 16:43:19 -0500

    added routing for pages and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -7,6 +8,7 @@ import { PatientsComponent } from './pages/patients/patients.component';
 import { LabsComponent } from './pages/labs/labs.component';
 import { PharmacistsComponent } from './pages/pharmacists/pharmacists.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +17,18 @@ import { HomeComponent } from './pages/home/home.component';
     PatientsComponent,
     LabsComponent,
     PharmacistsComponent,
-    HomeComponent
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'patients', component: PatientsComponent },
+      { path: 'labs', component: LabsComponent },
+      { path: 'pharmacists', component: PharmacistsComponent },
+      { path: '**', component: NotFoundComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
