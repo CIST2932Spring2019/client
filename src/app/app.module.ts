5cf143ff8784bb582d2c7b5a17624e8ac5795350
CIST2932Spring2019 <william.ith13@gmail.com>
2019-02-12 15:58:56 -0500

    added folders for web pages and shared components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { PatientsComponent } from './pages/patients/patients.component';
+import { LabsComponent } from './pages/labs/labs.component';
+import { PharmacistsComponent } from './pages/pharmacists/pharmacists.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PatientsComponent,
+    LabsComponent,
+    PharmacistsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
