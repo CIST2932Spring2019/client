4d90e9717ceff33999f12dee61cbe7c238426b42
CIST2932Spring2019 <william.ith13@gmail.com>
2019-03-16 16:35:25 -0400

    can now POST labs

--- src/app/pages/patients/view-all-patients/patient-profile/create-patient-lab/create-patient-lab.component.ts
+++ src/app/pages/patients/view-all-patients/patient-profile/create-patient-lab/create-patient-lab.component.ts
@@ -1,8 +1,7 @@
 import { CurrentPatientService } from './../../../../../shared/services/current-patient/current-patient.service';
 import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
 import { GetAllLabTypesService } from 'src/app/shared/services/get-all-lab-types/get-all-lab-types.service';
-
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'create-patient-lab',
@@ -11,10 +10,14 @@ import { GetAllLabTypesService } from 'src/app/shared/services/get-all-lab-types
 })
 export class CreatePatientLabComponent implements OnInit {
   patient: any;
-  labTypes: any[];
+  labTypes = [];
+  labs = [];
+  labsInitialized = false;
+
+  private url: string = 'http://localhost:44400/api/labscollection';
 
   constructor(
-    private location: Location,
+    private http: Http,
     private currentPatientService: CurrentPatientService,
     private getAllLabTypesService: GetAllLabTypesService) {
   }
@@ -25,10 +28,38 @@ export class CreatePatientLabComponent implements OnInit {
     this.getAllLabTypesService.getAllLabTypes()
       .subscribe(response => {
         this.labTypes = response.json();
-      })
+      });
   }
 
-  goBackClicked() {
-    this.location.back();
+  initializeLabs() {
+    let todayDate = new Date();
+    let todayDateISO = todayDate.toISOString();
+    if (this.labsInitialized === false) {
+      for (let index = 0; index < this.labTypes.length; index++) {
+        this.labs[index] = {
+          patientId: this.patient.patientId,
+          labType: this.labTypes[index].labType,
+          labValue: 0,
+          date: todayDateISO,
+          isHidden: false
+        };
+      };
+      this.labsInitialized = true;
+    }
+  }
+
+  onValueChanged(index: any, event: any) {
+    this.labs[index].labValue = parseFloat(event.target.value);
+  }
+
+  postLabs() {
+    for (let index = 0; index < this.labs.length; index++) {
+      if (this.labs[index].labValue > 0) {
+        this.http.post(this.url, this.labs[index])
+          .subscribe(response => {
+            console.log(response.json());
+        });
+      }
+    }
   }
 }
