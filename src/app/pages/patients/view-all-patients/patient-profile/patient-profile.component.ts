b053fc6a1dde8b19f36e21b9e2288237183894e0
CIST2932Spring2019 <william.ith13@gmail.com>
2019-03-18 22:43:48 -0400

    working dev version

--- src/app/pages/patients/view-all-patients/patient-profile/patient-profile.component.ts
+++ src/app/pages/patients/view-all-patients/patient-profile/patient-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GetPatientLabsService } from 'src/app/shared/services/get-patient-labs/get-patient-labs.service';
 import { CurrentPatientService } from 'src/app/shared/services/current-patient/current-patient.service';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'patient-profile',
@@ -10,12 +11,14 @@ import { CurrentPatientService } from 'src/app/shared/services/current-patient/c
 export class PatientProfileComponent implements OnInit {
   patient: any;
   labs: any[];
-
   isVisible = true;
+
+  private url: string = 'http://localhost:44400/api/labscollection';
 
   constructor(
     private currentPatientService: CurrentPatientService,
-    private getPatientLabsService: GetPatientLabsService) {
+    private getPatientLabsService: GetPatientLabsService,
+    private http: Http) {
   }
 
   ngOnInit() {
@@ -25,5 +28,13 @@ export class PatientProfileComponent implements OnInit {
       .subscribe(response => {
         this.labs = response.json();
       })
+  }
+
+  softDeleteLab(index: any) {
+    this.labs[index].labValue = 123;
+    this.http.put(this.url + '/' + this.labs[index].id, this.labs[index])
+      .subscribe(response => {
+        console.log(response.json());
+      });
   }
 }
